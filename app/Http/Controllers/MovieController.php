18788de8d29d4ce062d441086668fe414762537d
Faris Hasim <farishasimsyauqi@gmail.com>
2023-11-10 08:42:08 +0700

    feat(home): update home page
    Update home page to use real data fetched from api
    
    Closes #1

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -8,22 +8,37 @@ use Inertia\Inertia;
 
 class MovieController extends Controller
 {
-    //
-    public function index()
-    {
-        $client = new Client();
+    private $client;
 
-        $response = $client->request('GET', 'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', [
+    public function __construct() {
+        $this->client = new Client();
+    }
+
+    private function fetch(string $url)
+    {
+        $response = $this->client->request('GET', $url, [
             'headers' => [
               'Authorization' => "Bearer " . config('services.tmdb.key'),
               'accept' => 'application/json',
             ],
         ]);
 
-        // $responseBody = json_decode($response->getBody());
+        return json_decode($response->getBody());
+    }
+
+    //
+    public function index()
+    {
+        $now_playing = $this->fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1');
+        $popular = $this->fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1');
+        $top_rated = $this->fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1');
+        $upcoming = $this->fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1');
 
         return Inertia::render('Movies/Index', [
-            'response' => json_decode($response->getBody()),
+            'now_playing' => $now_playing,
+            'popular' => $popular,
+            'top_rated' => $top_rated,
+            'upcoming' => $upcoming,
         ]);
         // return json_decode($response->getBody());
     }
--- resources/js/Pages/Movies/Index.vue
+++ resources/js/Pages/Movies/Index.vue
@@ -3,8 +3,9 @@ import HomeLayout from '@/Layouts/HomeLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { useForm, Head } from '@inertiajs/vue3';
+import Card from '@/Components/Card.vue';
 
-defineProps(['response']);
+defineProps(['now_playing', 'popular', 'top_rated', 'upcoming']);
 </script>
  
 <template>
@@ -14,9 +15,8 @@ defineProps(['response']);
         <div class="m-10">
             <h3 class="text-xl font-semibold text-gray-800">Now Playing</h3>
             <div class="flex flex-wrap justify-start space-x-4 pt-4">
-                <!-- Loop through the cardData array and render a Card component for each item -->
                 <Card
-                v-for="(card, index) in response.results.slice(0,4)"
+                v-for="(card, index) in now_playing.results.slice(0,4)"
                 :key="index"
                 :title="card.title"
                 :description="card.description"
@@ -27,78 +27,38 @@ defineProps(['response']);
         <div class="m-10">
             <h3 class="text-xl font-semibold text-gray-800">Popular</h3>
             <div class="flex flex-wrap justify-start space-x-4 pt-4">
-                <!-- Loop through the cardData array and render a Card component for each item -->
-                <Card 
-                v-for="(card, index) in cardData"
+                <Card
+                v-for="(card, index) in popular.results.slice(0,4)"
                 :key="index"
                 :title="card.title"
                 :description="card.description"
-                :imageSrc="card.imageSrc"
+                :imageSrc="`https://image.tmdb.org/t/p/w500/${card.poster_path}`"
                 />
             </div>
         </div>
         <div class="m-10">
             <h3 class="text-xl font-semibold text-gray-800">Top Rated</h3>
             <div class="flex flex-wrap justify-start space-x-4 pt-4">
-                <!-- Loop through the cardData array and render a Card component for each item -->
-                <Card 
-                v-for="(card, index) in cardData"
+                <Card
+                v-for="(card, index) in top_rated.results.slice(0,4)"
                 :key="index"
                 :title="card.title"
                 :description="card.description"
-                :imageSrc="card.imageSrc"
+                :imageSrc="`https://image.tmdb.org/t/p/w500/${card.poster_path}`"
                 />
             </div>
         </div>
         <div class="m-10">
             <h3 class="text-xl font-semibold text-gray-800">Upcoming</h3>
             <div class="flex flex-wrap justify-start space-x-4 pt-4">
-                <!-- Loop through the cardData array and render a Card component for each item -->
-                <Card 
-                v-for="(card, index) in cardData"
+                <Card
+                v-for="(card, index) in upcoming.results.slice(0,4)"
                 :key="index"
                 :title="card.title"
                 :description="card.description"
-                :imageSrc="card.imageSrc"
+                :imageSrc="`https://image.tmdb.org/t/p/w500/${card.poster_path}`"
                 />
             </div>
         </div>
     </HomeLayout>
 </template>
-
-<script>
-import Card from '@/Components/Card.vue';
-
-export default {
-  components: {
-    Card,
-  },
-  data() {
-    return {
-      cardData: [
-        {
-          title: 'Card 1',
-          description: 'Description for Card 1.',
-          imageSrc: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
-        },
-        {
-          title: 'Card 2',
-          description: 'Description for Card 2.',
-          imageSrc: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
-        },
-        {
-          title: 'Card 3',
-          description: 'Description for Card 3.',
-          imageSrc: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
-        },
-        {
-          title: 'Card 4',
-          description: 'Description for Card 4.',
-          imageSrc: 'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
-        },
-        // Add more card data as needed
-      ],
-    };
-  },
-};
-</script>
